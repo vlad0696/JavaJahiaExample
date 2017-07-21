aa05290685d4c76a272af0029c3c68e0de717f89
Gramovich_V <vladislav-gramovich@mail.ru>
2017-07-21 11:45:34 +0300

    java example

---
+++ src/main/java/org/jahia/modules/examples/springmvc/Address.java
@@ -0,0 +1,29 @@
+package org.jahia.modules.examples.springmvc;
+
+import java.io.Serializable;
+
+/**
+ * Created by Gramovich_V on 20.07.2017.
+ */
+public class Address implements Serializable {
+
+    private  String city;
+
+    private  int   id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- src/main/java/org/jahia/modules/examples/springmvc/ExampleController.java
+++ src/main/java/org/jahia/modules/examples/springmvc/ExampleController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A basic example of how to use Spring MVC annotated controllers with the Jahia CMS.
  */
@@ -31,14 +36,58 @@ public class ExampleController {
             return lastName;
         }
     }
-
-    @RequestMapping(method= RequestMethod.GET,value="/hello",produces=MediaType.TEXT_PLAIN_VALUE)
-    public @ResponseBody String getHello() {
-        return "Hello World !";
+    public static Connection getPostgresConnection() {
+        try {
+            DriverManager.registerDriver((Driver) Class.forName("org.postgresql.Driver").newInstance());
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Test","postgres", "qwerty123");
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+ //   @RequestMapping(method= RequestMethod.GET,value="/hello",produces=MediaType.TEXT_PLAIN_VALUE)
+    public String getHello() throws SQLException {
+        Connection connection = getPostgresConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs =stmt.executeQuery("select id, city from public.address;");
+        Map<Integer, String> entityList = new HashMap<Integer, String>();
+        String bd=new String();
 
-    @RequestMapping(method= RequestMethod.GET,value="/hello/{world}",produces=MediaType.TEXT_PLAIN_VALUE)
-    public @ResponseBody String getHello(@PathVariable String world) {
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  address = rs.getString("city");
+            bd+= Integer.toString(id)+" : " +address +"\n";
+            entityList.put(id, address);
+
+        }
+
+        return bd;
+    }
+    public ArrayList<Address> getHello1() throws SQLException {
+        Connection connection = getPostgresConnection();
+        Statement stmt = connection.createStatement();
+        ResultSet rs =stmt.executeQuery("select id, city from public.address;");
+        ArrayList<Address> list = new ArrayList<Address>();
+
+        while ( rs.next() ) {
+            int id = rs.getInt("id");
+            String  city = rs.getString("city");
+            Address address= new Address();
+            address.setId(id);
+            address.setCity(city);
+            list.add(address);
+        }
+
+        return list;
+    }
+     public String getHello( String world) {
         return "Hello " + world;
     }
 
